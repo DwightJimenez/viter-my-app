f61411b23b2ef4aaa40a72e440a4aaf4c10e7e07
Jude Dwight <dwightjimenez0@gmail.com>
2025-07-17 15:45:11 +0800

    refactor: restructure home page components and update import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Header from "./components/partials/Header";
 import Footer from "./components/partials/Footer";
-import Home from "./components/pages/home/Home";
+import Home from "./components/pages/website/home/Home";
 
 const App = () => {
   return (
